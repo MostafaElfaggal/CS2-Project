5b15586f3488080162f9b301152e18365fa1bd0f
MostafaElfaggal <mostafaelfaggal@aucegypt.edu>
2022-04-24 01:39:53 +0200

    horus images

--- enemy1.cpp
+++ enemy1.cpp
@@ -2,10 +2,23 @@
 
 Enemy1::Enemy1(int x_pos, int y_pos) : Enemy(x_pos, y_pos, "Enemy.png")
 {
+    // define animations
+    for (int i=0; i<9; i++)
+    {
+        a1.push_back(QPixmap("Horus" + QString::number(i+1) + ".png"));
+        a1[i] = a1[i].scaledToHeight(50);
+        a1[i] = a1[i].scaledToWidth(50);
+    }
+
+    animations = &a1;
 }
 
 void Enemy1::update(int frame)
 {
+    if (frame%2 == 0) {
+        animate(); // use the animate function every second which will auto scroll through the different animations
+    }
+
     if (frame % 6 == 0)
     {
         int i = rand() % 4;
